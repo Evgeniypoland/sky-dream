e981f82da0c0bb3d0a65c10500fc0794e6cf0dfe
Evgeniy <pl01112022@gmail.com>
2023-09-20 23:59:18 +0200

    change

--- sky_dream/settings.py
+++ sky_dream/settings.py
@@ -154,8 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -174,3 +173,5 @@ CACHES = {
     }
 }
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
